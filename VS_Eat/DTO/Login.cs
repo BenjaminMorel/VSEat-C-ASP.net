ca3e6c5dcc49849c872f3ce0c172cb2243312df5
Benjamin Morel <benjamin.morel@hes-so.ch>
2021-12-12 19:51:21 +0100

    test map

--- VS_Eat/DTO/Login.cs
+++ VS_Eat/DTO/Login.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ namespace DTO
 
         public string Username{ get; set; }
 
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         public int IdLoginType { get; set; }
